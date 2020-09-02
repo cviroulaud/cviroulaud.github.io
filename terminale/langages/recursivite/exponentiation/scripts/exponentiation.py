0b65364630e78dfcf5ad9ec474c89520bbb6f4e4
Christophe Viroulaud <viroulaudchristophe@gmail.com>
2020-09-02 13:31:19 +0200

    correction QCM terminale

--- terminale/langages/recursivite/exponentiation/scripts/exponentiation.py
+++ terminale/langages/recursivite/exponentiation/scripts/exponentiation.py
@@ -43,11 +43,11 @@ def puissance_iteratif_rapide(x,n):
     res=1
     while n>0:
         if n % 2 == 0:
-            x = x**2
+            x = x*x
             n = n // 2
         else:
             res = res * x
-            n = n -1
+            n = n - 1
     return res
 
 from time import time

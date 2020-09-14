9b85db4f1c3b3cc50ba38f487fe63fda61ff2304
Christophe Viroulaud <viroulaudchristophe@gmail.com>
2020-09-14 22:52:33 +0200

    semaine 3 - corrigé exo term

---
+++ terminale/langages/paradigmes/POO-TP-banque/scripts/modularite_poo.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Date de création Fri Sep 11 08:42:32 2020
+
+@auteur: Christophe Viroulaud
+"""
+
+
+from mod_temperature import Temperature
+help(Temperature)

600173349503bdfe4641c7a3d6f4b0043ddbd466
Christophe Viroulaud <viroulaudchristophe@gmail.com>
2020-09-15 21:59:35 +0200

    up 1ere

--- custom_slides.js
+++ custom_slides.js
@@ -1,23 +1,4 @@
 document.body.addEventListener('click', function(){
+//Reveal.nextFragment();
     Reveal.toggleOverview();
-Reveal.on( 'fragmentshown', event => {
-  // event.fragment = the fragment DOM element
-alert();
-} );
 }); 
-
-$(document).ready(function(){
-alert();
-Reveal.configure({
-        transition: 'convex' // none/fade/slide/convex/concave/zoom
-    })
-	
-)};
-Reveal.on( 'ready', event => {
-  // event.currentSlide, event.indexh, event.indexv
-alert("test");
-} );
-Reveal.on( 'slidechanged', event => {
-  // event.previousSlide, event.currentSlide, event.indexh, event.indexv
-
-} );

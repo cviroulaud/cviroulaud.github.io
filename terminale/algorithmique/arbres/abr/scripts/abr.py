10d53aebc9f0be8bd1dbc37974a60f33b534dd92
Christophe Viroulaud <viroulaudchristophe@gmail.com>
2021-02-22 12:58:08 +0100

    up cdm2018 term

---
+++ terminale/algorithmique/arbres/abr/scripts/abr.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Date de création Mon Feb 22 10:28:12 2021
+
+@auteur: Christophe Viroulaud
+"""
+
+
+class Noeud:
+
+    def __init__(self, v, g = None, d = None):
+        self.valeur = v
+        self.gauche = g
+        self.droite = d
+
+    def inserer(self, v: int)->None:
+        """
+        crée un Noeud dans le sous-arbre gauche ou droit
+        """
+        if v < self.valeur:
+            if self.gauche is None:
+                self.gauche = Noeud(v)
+            else:
+                self.gauche.inserer(v)
+        elif v > self.valeur: # permet de ne pas ajouter une valeur déjà présente
+            if self.droite is None:
+                self.droite = Noeud(v)
+            else:
+                self.droite.inserer(v)
+
+    def rechercher(self, v: int)->bool:
+        """
+        recherche v dans les sous-arbre gauche ou droit
+        """
+        if v == self.valeur:
+            return True
+        elif v < self.valeur:
+            if self.gauche is None:
+                return False
+            else:
+                return self.gauche.rechercher(v)
+        elif v > self.valeur:
+            if self.droite is None:
+                return False
+            else:
+                return self.droite.rechercher(v)
+
+
+class ABR:
+
+    def __init__(self, r = None):
+        self.racine = r
+
+    def est_vide(self)->bool:
+        return self.racine is None
+
+    def inserer(self, v: int)->None:
+        """
+        insère v dans l'ABR
+        en appelant la méthode inserer de Noeud
+        """
+        if self.racine is None:
+            # création de la racine
+            self.racine = Noeud(v)
+        else:
+            # appel de la méthode inserer de Noeud
+            self.racine.inserer(v)
+
+    def rechercher(self, v: int)->bool:
+        """
+        recherche v dans l'ABR
+        en appelant la méthode rechercher de Noeud
+        """
+        if self.racine is None:
+            return False
+        else:
+            return self.racine.rechercher(v)
+
+    def infixe_rec(self, n: Noeud, parcours: list)->list:
+        """
+        méthode interne du parcours infixe
+        """
+        if n is None:
+            return
+        self.infixe_rec(n.gauche, parcours)
+        parcours.append(n.valeur)
+        self.infixe_rec(n.droite, parcours)
+        return parcours
+
+    def infixe(self)->list:
+        return self.infixe_rec(self.racine, [])
+
+tab = [33, 25, 56, 20, 28, 40, 60, 8, 21, 26, 35, 58, 65]
+arbre = ABR()
+for e in tab:
+    arbre.inserer(e)
+print(arbre.infixe())
+print(arbre.rechercher(21))
+print(arbre.rechercher(61))

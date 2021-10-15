c7e3183b594bbea2a028c52dfe24c0987e97b9fd
Christophe Viroulaud <viroulaudchristophe@gmail.com>
2021-10-15 22:57:16 +0200

    corrections

--- terminale/langages/paradigmes/POO/exercices/scripts/date_v2.py
+++ terminale/langages/paradigmes/POO/exercices/scripts/date_v2.py
@@ -8,24 +8,25 @@ Date de création Fri Aug 21 10:10:53 2020
 
 
 class Date:
+
+    # on définit une variable interne à la classe
+    nom_mois = ["janvier", "février", "mars", "avril", "mai", "juin",
+                "juillet", "août", "septembre", "octobre", "novembre", "décembre"]
+
     def __init__(self, j: int, m: int, a: int):
         self.jour = j
         self.mois = m
         self.annee = a
 
     def afficher(self) -> str:
-        nom_mois = ["janvier", "février", "mars", "avril", "mai", "juin",
-                "juillet", "août", "septembre", "octobre", "novembre", "décembre"]
-        return f"{self.jour} / {nom_mois[self.mois - 1]} / {self.annee}"
+        return f"{self.jour} / {Date.nom_mois[self.mois - 1]} / {self.annee}"
 
     def est_avant(self, d) -> bool:
         # Le \ permet d'écrire sur plusieurs lignes
         # and est prioritaire devant or
         return self.annee < d.annee or \
-            self.annee == d.annee and \
-                (self.mois < d.mois or \
-                self.mois == d.mois and \
-                    self.jour < d.jour)
+            self.annee == d.annee and (self.mois < d.mois or \
+                                       self.mois == d.mois and self.jour < d.jour)
 
 
 d = Date(8, 12, 1977)
